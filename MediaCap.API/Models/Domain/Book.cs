df6a6fecc16679114672d4d9a890b206b06bbe46
Marius Kasparek <marius.kasparek@gmail.com>
2025-05-02 10:54:38 +0200

    Added Domain Models

---
+++ MediaCap.API/Models/Domain/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace MediaCap.API.Models.Domain
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Rating { get; set; }
+        public int Year { get; set; }
+        public int YearRead { get; set; }
+        public int MonthRead { get; set; }
+        public bool Favorite { get; set; }
+        public int Timestamp { get; set; }
+        public List<string> Genres { get; set; } = new List<string>();
+
+    }
+}
---
+++ MediaCap.API/Models/Domain/Series.cs
@@ -0,0 +1,15 @@
+﻿namespace MediaCap.API.Models.Domain
+{
+    public class Series
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Rating { get; set; }
+        public int Year { get; set; }
+        public int YearWatched { get; set; }
+        public int MonthWatched { get; set; }
+        public bool Favorite { get; set; }
+        public int Timestamp { get; set; }
+        public List<string> Genres { get; set; } = new List<string>();
+    }
+}
