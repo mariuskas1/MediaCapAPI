87bb744581e32cbd9110b4fd9f2749b27bc2c3e5
Marius Kasparek <marius.kasparek@gmail.com>
2025-05-02 15:45:38 +0200

    BooksController and BookDTO

---
+++ MediaCap.API/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using MediaCap.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaCap.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly MediacapDbContext dbContext;
+
+        public BooksController(MediacapDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
---
+++ MediaCap.API/Models/DTO/BookDTO.cs
@@ -0,0 +1,20 @@
+﻿using MediaCap.API.Models.Domain;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediaCap.API.Models.DTO
+{
+    public class BookDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Guid AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public int Rating { get; set; }
+        public int Year { get; set; }
+        public int YearRead { get; set; }
+        public int MonthRead { get; set; }
+        public bool Favorite { get; set; }
+        public int Timestamp { get; set; }
+        public List<string> Genres { get; set; } = new List<string>();
+    }
+}
