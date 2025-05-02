b4c21feaf92dc5203fdb9043efd8b8fdd1975a58
Marius Kasparek <marius.kasparek@gmail.com>
2025-05-02 11:35:22 +0200

    Added Database

---
+++ MediaCap.API/Data/MediacapDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MediaCap.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaCap.API.Data
+{
+    public class MediacapDbContext: DbContext
+    {
+        public MediacapDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Book> Books{ get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Series> Series { get; set; }
+
+    }
+}
---
+++ MediaCap.API/Migrations/20250502093335_Initial Migration.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaCap.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    YearRead = table.Column<int>(type: "int", nullable: false),
+                    MonthRead = table.Column<int>(type: "int", nullable: false),
+                    Favorite = table.Column<bool>(type: "bit", nullable: false),
+                    Timestamp = table.Column<int>(type: "int", nullable: false),
+                    Genres = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    YearWatched = table.Column<int>(type: "int", nullable: false),
+                    MonthWatched = table.Column<int>(type: "int", nullable: false),
+                    Favorite = table.Column<bool>(type: "bit", nullable: false),
+                    Theatre = table.Column<bool>(type: "bit", nullable: false),
+                    Timestamp = table.Column<int>(type: "int", nullable: false),
+                    Genres = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    YearWatched = table.Column<int>(type: "int", nullable: false),
+                    MonthWatched = table.Column<int>(type: "int", nullable: false),
+                    Favorite = table.Column<bool>(type: "bit", nullable: false),
+                    Timestamp = table.Column<int>(type: "int", nullable: false),
+                    Genres = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Films");
+
+            migrationBuilder.DropTable(
+                name: "Series");
+        }
+    }
+}
