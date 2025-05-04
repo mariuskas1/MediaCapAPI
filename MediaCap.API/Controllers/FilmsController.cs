7f717dd562bbbeb447fea3098abafae2d7fa517d
Marius Kasparek <marius.kasparek@gmail.com>
2025-05-04 09:58:49 +0200

    FilmsController Methods

--- MediaCap.API/Controllers/FilmsController.cs
+++ MediaCap.API/Controllers/FilmsController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using MediaCap.API.Data;
+using MediaCap.API.Models.Domain;
+using MediaCap.API.Models.DTO;
 using MediaCap.API.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,5 +23,59 @@ namespace MediaCap.API.Controllers
             this.mapper = mapper;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] FilmDTO filmDTO)
+        {
+            var filmDomainModel = mapper.Map<Film>(filmDTO);
+            await filmRepository.CreateAsync(filmDomainModel);
+            return Ok(mapper.Map<FilmDTO>(filmDomainModel));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var filmsDomainModel = await filmRepository.GetAllAsync();
+            return Ok(mapper.Map<List<FilmDTO>>(filmsDomainModel));
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var filmDomainModel = await filmRepository.GetByIdAsync(id);
+            if (filmDomainModel == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<FilmDTO>(filmDomainModel));
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, FilmDTO filmDTO)
+        {
+            var filmDomainModel = mapper.Map<Film>(filmDTO);
+            filmDomainModel = await filmRepository.UpdateAsync(id, filmDomainModel);
+
+            if(filmDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<FilmDTO>(filmDomainModel));
+        }
+
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var filmDomainModel = await filmRepository.DeleteAsync(id);
+            if (filmDomainModel == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<FilmDTO>(filmDomainModel));
+        }
     }
 }
