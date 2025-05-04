cf7caafd609be8d464f8be80c9583919d721917b
Marius Kasparek <marius.kasparek@gmail.com>
2025-05-04 09:48:22 +0200

    FilmsController

---
+++ MediaCap.API/Controllers/FilmsController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediaCap.API.Data;
+using MediaCap.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaCap.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmsController : ControllerBase
+    {
+        private readonly MediacapDbContext dbContext;
+        private readonly IFilmRepository filmRepository;
+        private readonly IMapper mapper;
+
+        public FilmsController(MediacapDbContext dbContext, IFilmRepository filmRepository, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.filmRepository = filmRepository;
+            this.mapper = mapper;
+        }
+
+    }
+}
